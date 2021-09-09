eb6bbf1cae50189672f16c59e8e8733579b36196
Steve Roche <sroche@gmail.com>
2021-09-09 13:04:28 -0700

    Fixed compass visibility bug

--- Tests/MapboxMapsTests/Ornaments/OrnamentSupportableViewMock.swift
+++ Tests/MapboxMapsTests/Ornaments/OrnamentSupportableViewMock.swift
@@ -4,11 +4,15 @@ import UIKit
 //swiftlint:disable explicit_acl explicit_top_level_acl
 // Mock class that flags true when `OrnamentSupportableView` protocol methods have been called on it
 class OrnamentSupportableViewMock: UIView, OrnamentSupportableView {
+    private var subscribers: [(CameraState) -> Void] = []
 
     func subscribeCameraChangeHandler(_ handler: @escaping (CameraState) -> Void) {
-
+        subscribers.append(handler)
     }
-
+    // Notifys all subscribers of a chamera changed event
+    func notifyCameraChanged(cameraState: CameraState) {
+        subscribers.forEach({ $0(cameraState) })
+    }
     var tapCalled: Bool = false
 
     func tapped() {
